7431e526a2ec0bae302e2e45976893e166be17a3
DjihaneB <bouizemdjihane1312@gmail.com>
2023-06-27 16:58:14 +0200

    Ajouter une image cover pour styliser la page Login

--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Bouton from '../../composants/bouton/Bouton';
 import { Link } from 'react-router-dom';
+import COVER_IMAGE from "./imageLogin.jpg";
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-      <Link to="/accueil"><Bouton texteBouton="Se connecter"/></Link>
+    <div className= "w-full min-h-screen flex items-start">
+      {/* faire une image de cover */}
+      <div className= 'relative w-1/2 h-full flex flex-col' >
+        <img src={COVER_IMAGE} className="w-full h-full object-cover "/>
+
+      </div>
+      {/* <div>
+        <h1>Login</h1>
+        <p>Welcome Back! Please entre your details</p>
+        <Link to="/accueil"><Bouton texteBouton="Se connecter"/></Link>
+      </div> */}
     </div>
   )
 }
