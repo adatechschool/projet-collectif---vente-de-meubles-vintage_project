f934bc953812dcc09ad37fc1f426fa45cd32f414
Lea Cassegrain <lea.csgn@gmail.com>
2023-06-27 16:01:49 +0200

    style : footer deuxième version

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'beige': "#F7F0ED",
+      },
+    },
   },
   plugins: [],
 }
