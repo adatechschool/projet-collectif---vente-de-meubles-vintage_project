a0d369ff95f6520af7347f1d3067015ed42f0ceb
Lea Cassegrain <lea.csgn@gmail.com>
2023-06-27 11:25:30 +0200

    Création des pages et des routes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Accueil from "./pages/Accueil/Accueil";
+import Admin from "./pages/Admin/Admin";
+import Panier from "./pages/Panier/Panier";
+import Produit from "./pages/Produit/Produit";
 import './App.css'
 
 function App() {
 
 
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}/> 
+      <Route path="/accueil" element={<Accueil/>}/>
+      <Route path="/admin" element={<Admin/>}/>
+      <Route path="/produit" element={<Produit/>}/>
+      <Route path="/panier" element={<Panier/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ front/src/pages/Accueil/Accueil.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Accueil() {
+  return (
+    <div>Accueil</div>
+  )
+}
