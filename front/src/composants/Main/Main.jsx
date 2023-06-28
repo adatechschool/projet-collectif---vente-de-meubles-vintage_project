a01781ddda88fa0e9e8c31755d6670d654672c23
Justine <justine-lambert@hotmail.fr>
2023-06-28 16:05:52 +0200

    Récupérer les infos des vignettes d'un fichier dans le dossier data et avec la fonction map

--- front/src/composants/Main/Main.jsx
+++ front/src/composants/Main/Main.jsx
@@ -1,25 +1,18 @@
 import React from 'react'
 import Vignette from '../Vignette/Vignette'
+import {produits} from '../../data/produits'
 
 function Main() {
   return (
-    <div className='h-96 flex'>
-      {function() {
-        let vignettes = [];
-        // Tableau "infos" fictif, renvoyé par le serveur
-        // A modifier quand on saura faire le lien avec des informations renvoyées par le serveur (back)
-        let infos = [{'nom': 'Meuble', 'prix': 30},
-         {'nom': 'Chaise', 'prix': 10},
-         {'nom': 'Commode', 'prix': 120}];
-        // Boucle for pour afficher plusieurs vignettes
-        for (let i=0; i<infos.length; i++) {
-          vignettes.push(<Vignette
-          nom={infos[i].nom}
-          prix={infos[i].prix}
-          />)
-        }
-        return vignettes;
-      }()
+    <div className='flex flex-wrap'>
+      {
+        produits.map(({ id, nom, prix }) => (
+          <Vignette
+            key={id}
+            nom={nom}
+            prix={prix} 
+          />
+        ))
       }
     </div>
   )
--- front/src/composants/Vignette/Vignette.jsx
+++ front/src/composants/Vignette/Vignette.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Bouton from '../bouton/Bouton'
 import noImage from '../../assets/img_vignette/img_non_dispo.jpg'
 
-function Vignette(props) {
+function Vignette({id, nom, prix}) {
   return (
-    <div className='Global h-80 w-72 p-2 m-2 bg-orange-500'>
+    <div key={id} className='Global h-80 w-72 p-2 m-2 bg-orange-500'>
         <div className='image h-4/6'>
             <img src={noImage} className='h-full w-full object-cover'/>
         </div>
         <div className='contenu bg-red-100 flex h-2/6'>
             <div className='produit bg-red-500 w-2/3 text-left p-2'>
-                <h1>{props.nom}</h1>
-                <p>{props.prix} €</p>
+                <h1>{nom}</h1>
+                <p>{prix} €</p>
             </div>
             <div className='acheter bg-red-200 w-1/3 p-2'>
                 <Bouton texteBouton="Acheter"/>
