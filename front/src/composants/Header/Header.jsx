8a6dd248f35c8821f1dffb3eb2836cb2e4918927
PierreJzql <pierrejezequel92@gmail.com>
2023-06-28 14:34:05 +0200

    style : photo bg unique et deplacement texte

--- front/src/composants/Header/Header.jsx
+++ front/src/composants/Header/Header.jsx
@@ -3,11 +3,21 @@ import './Header.css'
 
 function Header() {
   return (
-    <div className='header flex text-dark-brown h-screen'>
-        <div className='border-[1px] border-black text-start mx-auto my-auto py-4 px-2 bg-beige opacity-[.85]'>
-          <h1>Vintage :</h1>
-          <h2>Le mobilier qui a fait ses preuves !</h2>
+    <div className='header flex flex-col bg-cover text-dark-brown h-screen '>
+        <div className='border-[1px] border-black text-start mx-auto mt-8 mb-auto py-4 px-6 rounded-xl bg-beige opacity-[.85]'>
+          <h1 className='text-6xl'>Vintage :</h1>
+          <h2 className='text-4xl'>Le mobilier qui a fait ses preuves !</h2>
         </div>
+        
+        <div className="mt-auto">
+          <span class="inline-block">
+            <svg class="w-32 h-32 text-beige" fill="none" stroke="beige" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M19 9l-7 7-7-7"></path>
+            </svg>
+          </span>
+        </div>
+       
+
     </div>
   )
 }
