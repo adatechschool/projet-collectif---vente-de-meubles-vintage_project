a383eca5e7a504bd544cd68f01ac2e13747ee7e5
DjihaneB <bouizemdjihane1312@gmail.com>
2023-06-28 10:37:59 +0200

    Première vision éléments page login

--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -3,21 +3,41 @@ import Bouton from '../../composants/bouton/Bouton';
 import { Link } from 'react-router-dom';
 import COVER_IMAGE from "./imageLogin.jpg";
 
+const color = {
+  background: "#f5f5f5"
+}
+
 function Login() {
   return (
-    <div className= "w-full min-h-screen flex items-start">
-      {/* faire une image de cover */}
-      <div className= 'relative w-1/2 h-full flex flex-col' >
-        <img src={COVER_IMAGE} className="w-full h-full object-cover "/>
+    <div className= "w-full min-h-screen flex items-start bg-[#f5f5f5]">
+        <div className= 'relative w-1/2 h-full flex flex-col' >
+          <div className= 'absolute top-[20%] left-[10%] right-[10%] flex flex-col '>
+            <h1 className="text-4xl text-white font-bold my-4 ">Transformez votre intérieur avec nos meubles vintage uniques,</h1>
+            <p className="text-xl text-white font-normal">Commencez gratuitement et bénéficiez des offres attractives de la communauté. </p>
+          </div>
+          <img src={COVER_IMAGE} className="w-full h-full object-cover "/>
+        </div>
+        
+        <div className="w-1/4 h-full bg-[#f5f5f5] flex flex-col p-20 justify-between">
+          <h1 className="text-x1 text-[#060606] font-semibold">Vintage</h1>
 
-      </div>
-      {/* <div>
+          <div className='w-full flex flex-col'>
+            <h3 className="text-2xl font-semibold mb-4">Login</h3>
+            <p className="text-sm mb-2">Veuillez entrer vos informations.</p>
+          </div>
+
+          {/* <div className ="w-full">
+            <p className="text-sm font-normal text-[#060606]">Vous n&#39;avez pas encore de compte? <span className='font-semibold underline underline-offse'></span></p>
+          </div> */}
+
+
+        </div>
+    </div>
+  )
+}
+{/* <div>
         <h1>Login</h1>
         <p>Welcome Back! Please entre your details</p>
         <Link to="/accueil"><Bouton texteBouton="Se connecter"/></Link>
       </div> */}
-    </div>
-  )
-}
-
 export default Login;
