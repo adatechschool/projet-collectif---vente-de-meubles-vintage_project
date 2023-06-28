4b94231d820b09c74352d934435589e7176ae815
Coquelicot49 <coraline.jadeau@gmail.com>
2023-06-28 15:04:08 +0200

    style : navbar

--- front/src/composants/Navbar/Navbar.jsx
+++ front/src/composants/Navbar/Navbar.jsx
@@ -3,6 +3,10 @@ import logoSun from "/src/assets/logo-sun.png"
 import logoPanier from "/src/assets/icon_panier.png"
 import menuIcon from "/src/assets/menu_icon.png"
 import { Link } from 'react-router-dom'
+import Menu from '../Menu/Menu'
+import LocalMallIcon from '@mui/icons-material/LocalMall';
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 
 function Navbar() {
@@ -15,13 +19,18 @@ function Navbar() {
 
   return (
     <div className='bg-beige h-32' style={backgroundStyle}>
-      <ul className='flex items-center justify-between'>
-        <li className='bg-lime-600 text-3xl text-black-600 font-bold'>Vintage</li>
-        {/* <li className='flex-2'><img src={logoSun}></img></li> */}
-        <li className='flex-3'><Link to="/panier"><img src={logoPanier}></img></Link></li>
-        <li className='flex-4'><img src={menuIcon}></img></li>
+      <ul className='flex h-full '>
+        <div className='h-full flex justify-start items-center w-3/4'>
+          <li className='ml-[150px] text-3xl text-black-600 font-bold'>Vintage</li>
+        </div>
+        <div className='flex justify-end items-center w-1/4'>
+          <li className='flex-3 mr-2.5'><Link to="/panier" className='text-black'>
+            <LocalMallIcon sx={{fontSize:70}}/></Link>
+            </li>
+          <li className='flex-3 mt-3'><MenuIcon sx={{fontSize:70}}/></li>
+          {/* <li><Menu/></li> */}
+        </div>
       </ul>
-    
     </div>
 
   )
