3fe92714b95f149319e60a7bf1cc753c321bf37f
Elodieguay <e.guayroso@gmail.com>
2023-06-29 16:17:27 +0200

    essai front

--- front/src/composants/Main/Main.jsx
+++ front/src/composants/Main/Main.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Vignette from '../Vignette/Vignette'
-import {produits} from '../../data/produits'
+// import {produits} from '../../data/produits'
+
 
 function Main() {
+
+  const [meuble, setMeuble] = useState()
+
+  useEffect ( ()=>{
+    getMeubles()
+
+  }, [])
+
+  const getMeubles = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/meubles');
+      const jsonMeuble = await response.json();
+      setMeuble(jsonMeuble);
+    } catch(error) {
+      console.log('Error', error);
+    }
+  }
+
   return (
     <div id="main" className='flex flex-wrap min-h-screen'>
-      {
+      <div>bonjour</div>
+
+  {
+        meuble.map(item => (
+          <Vignette
+            key={item.id}
+            nom={item.nom}
+            prix={item.prix} 
+          />
+        ))
+      }
+
+
+      {/* {
         produits.map(({ id, nom, prix }) => (
           <Vignette
             key={id}
@@ -13,7 +45,7 @@ function Main() {
             prix={prix} 
           />
         ))
-      }
+      } */}
     </div>
   )
 }
