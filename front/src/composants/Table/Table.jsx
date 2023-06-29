2fd57becf84afba3cabd0cb5c87cd8f217cfdac8
PierreJzql <pierrejezequel92@gmail.com>
2023-06-29 17:22:13 +0200

    style : update table admin

--- front/src/composants/Table/Table.jsx
+++ front/src/composants/Table/Table.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Bouton from '../Bouton/Bouton'
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import EditIcon from '@mui/icons-material/Edit';
 
 function Table({data}) {
   return (
     <div>
-        <table className='border-4 table-auto'>
-            <thead class="text-red-600">
+        <table className='border-4 table-auto min-h-screen mx-auto my-20'>
+            <thead className="text-red-600">
                 <tr>
                     <th className='border-2 text-2xl'>Nom</th>
                     <th className='border-2 text-2xl'>Prix</th>
@@ -15,18 +17,25 @@ function Table({data}) {
             <tbody className='border-4'>
                 {
                     data.map(el => (
-                        <tr>
-                            <td className='border-2 px-2'>{el.nom}</td>
-                            <td className='border-2 px-2'>{el.prix} €</td>
-                            <td className='flex mx-2'>
-                            <div className='mr-2'>
-                                <Bouton texteBouton="Supprimer" />
-                            </div>
-                            <div className="ml-2">
-                                <Bouton texteBouton="Modifier" />
-                            </div>
-                            </td>
-                        </tr>
+                            <tr key={el.id} className="border-2">
+                                <td className='px-2'>{el.nom}</td>
+                                <td className='border-2 px-2'>{el.prix} €</td>
+                                
+                                <div className='flex justify-center items-center'>
+                                <td className='flex mx-2 my-auto'>
+                                    <div className='mr-2 '>
+                                        <DeleteForeverIcon className="text-red-600" sx={{fontSize:50}}/>
+                                        {/* <Bouton texteBouton="Supprimer" /> */}
+                                    </div>
+                                    <div className="ml-2">
+                                        <EditIcon sx={{fontSize:50}}/>
+                                        {/* <Bouton texteBouton="Modifier" /> */}
+                                    </div>
+                                    </td>
+                                </div>
+                                
+                                
+                            </tr>
                     ))
                 }
                 
--- front/src/pages/Admin/Admin.jsx
+++ front/src/pages/Admin/Admin.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../composants/Navbar/Navbar';
 import Table from '../../composants/Table/Table';
+import Footer from '../../composants/Footer/Footer'
 import { produits } from '../../data/produits';
 
 function Admin() {
@@ -8,6 +9,7 @@ function Admin() {
     <div>
       <Navbar/>
       <Table data={produits}/>
+      <Footer/>
     </div>
   )
 }
