4682aa5fc5b6959777a0c67a45f9e8f5e65182a5
Coquelicot49 <coraline.jadeau@gmail.com>
2023-06-29 11:11:27 +0200

    fonction lien entre vignette et page produit détaillé + ajout navbar

--- front/src/composants/Vignette/Vignette.jsx
+++ front/src/composants/Vignette/Vignette.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import Bouton from '../Bouton/Bouton'
-import noImage from '../../assets/img_vignette/img_non_dispo.jpg'
+import React from 'react';
+import Bouton from '../Bouton/Bouton';
+import noImage from '../../assets/img_vignette/img_non_dispo.jpg';
+import { Link } from 'react-router-dom';
 
-function Vignette({id, nom, prix}) {
+function Vignette({ id, nom, prix }) {
   return (
     <div key={id} className='Global h-80 w-72 p-2 m-2 bg-orange-500'>
-        <div className='image h-4/6'>
-            <img src={noImage} className='h-full w-full object-cover'/>
+      <div className='image h-4/6'>
+        <Link to="/produit">
+          <img src={noImage} className='h-full w-full object-cover' alt='No Image' />
+        </Link>
+      </div>
+      <div className='contenu bg-red-100 flex h-2/6'>
+        <div className='produit bg-red-500 w-2/3 text-left p-2'>
+          <h1>{nom}</h1>
+          <p>{prix} €</p>
         </div>
-        <div className='contenu bg-red-100 flex h-2/6'>
-            <div className='produit bg-red-500 w-2/3 text-left p-2'>
-                <h1>{nom}</h1>
-                <p>{prix} €</p>
-            </div>
-            <div className='acheter bg-red-200 w-1/3 p-2'>
-                <Bouton texteBouton="Acheter"/>
-            </div>
+        <div className='acheter bg-red-200 w-1/3 p-2'>
+          <Bouton texteBouton="Acheter" />
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Vignette
+export default Vignette;
--- front/src/pages/Produit/Produit.jsx
+++ front/src/pages/Produit/Produit.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../../composants/Navbar/Navbar';
 
 function Produit() {
   return (
-    <div>Produit</div>
+    <div>
+      <Navbar/>
+      ceci est la page d'un produit détaillé
+    </div>
   )
 }
 
