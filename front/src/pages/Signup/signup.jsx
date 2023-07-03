b6b53367c0270edeff7e17b6fb236b76bfd74c4e
Nathan7594 <nathan.75@outlook.com>
2023-07-03 16:33:18 +0200

    push test

--- front/src/pages/Signup/signup.jsx
+++ front/src/pages/Signup/signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-const port=import.meta.env.VITE_PORT;
+const port = import.meta.env.VITE_PORT;
 
 function Signup() {
   const [name, setName] = useState("");
